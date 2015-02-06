f96155b1bbb582e31cefaff2389e83e5ec2f7749
amm <amm@amm-VirtualBox>
2015-02-06 23:46:52 +0100

    css html php

--- poddaLorenzo/php/view/master.php
+++ poddaLorenzo/php/view/master.php
@@ -24,7 +24,7 @@ if (!$vd->isJson()) {
         <head>
             <meta http-equiv="content-type" content="text/html; charset=utf-8" />
             <title><?= $vd->getTitolo() ?></title>
-           
+           <base href="<?= Settings::getApplicationPath() ?>php/"/>
             <meta name="keywords" content="AMM esami docente" />
             <meta name="description" content="Una pagina per gestire le funzioni dei docenti" />
             <link href="../css/stile.css" rel="stylesheet" type="text/css" media="screen" />

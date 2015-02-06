2a1a58885b41a25dd989aeeed5d80760e0c9d584
amm <amm@amm-VirtualBox>
2015-02-06 15:40:28 +0100

    css html php

--- poddaLorenzo/php/view/master.php
+++ poddaLorenzo/php/view/master.php
@@ -2,7 +2,7 @@
 include_once 'ViewDescriptor.php';
 include_once basename(__DIR__) . '/../Settings.php';
 
-//if (!$vd->isJson()) {
+if (!$vd->isJson()) {
     ?>
     <!DOCTYPE html>
     <!-- 
@@ -73,7 +73,7 @@ include_once basename(__DIR__) . '/../Settings.php';
         </body>
     </html>
     <?php
-/**} else {
+} else {
 
     header('Cache-Control: no-cache, must-revalidate');
     header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
@@ -81,7 +81,7 @@ include_once basename(__DIR__) . '/../Settings.php';
     
     $content = $vd->getContentFile();
     require "$content";
-}**/
+}
 ?>
 
 

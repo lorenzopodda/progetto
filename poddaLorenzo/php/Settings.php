39861361d8e5547a329b895bee354cd7ebb4a946
amm <amm@amm-VirtualBox>
2015-02-07 15:55:05 +0100

    css html php

--- poddaLorenzo/php/Settings.php
+++ poddaLorenzo/php/Settings.php
@@ -26,7 +26,7 @@ class Settings {
                     break;
                 case 'spano.sc.unica.it':
                     // configurazione pubblica
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/davide/esami14/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2014/davide/esami14/lorenzoPodda/poddaLorenzo/';
                     break;
 
                 default:

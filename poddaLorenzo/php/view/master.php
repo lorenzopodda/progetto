cb3f37f7f345a396e8daceefbc0dd726a2b130a0
amm <amm@amm-VirtualBox>
2015-02-07 15:18:55 +0100

    css html php

--- poddaLorenzo/php/view/master.php
+++ poddaLorenzo/php/view/master.php
@@ -35,8 +35,8 @@ include_once basename(__DIR__) . '/../Settings.php';
             <div id="pagina">
                 <div id="titolo">
                  <?php
-                        $titolo = $vd->getTitoloFile();
-                        require "$titolo";
+                        $titoloFile = $vd->getTitoloFile();
+                        require "$titoloFile";
                 ?>
 		    
              </div>

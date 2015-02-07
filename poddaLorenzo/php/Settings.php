c639ac50457a1b4e0fdcbf30c5e36d31b9ceab00
amm <amm@amm-VirtualBox>
2015-02-07 14:36:40 +0100

    css html php

--- poddaLorenzo/php/Settings.php
+++ poddaLorenzo/php/Settings.php
@@ -22,7 +22,7 @@ class Settings {
             switch ($_SERVER['HTTP_HOST']) {
                 case 'localhost':
                     // configurazione locale
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/esAMM2014/esami14/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/progetto/poddaLorenzo/';
                     break;
                 case 'spano.sc.unica.it':
                     // configurazione pubblica

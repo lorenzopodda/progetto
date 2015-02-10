1abcb3f43b0b9c29d352759054c9f5a7af475d10
amm <amm@amm-VirtualBox>
2015-02-10 16:18:45 +0100

    css html php

--- poddaLorenzo/php/error.php
+++ poddaLorenzo/php/error.php
@@ -16,7 +16,7 @@ include_once 'Settings.php';
             ?>
         </p>
         <?php if (isset($login)) { ?>
-            <p>Puoi autenticarti alla pagina di <a href="login">login</a></p>
+            <p>Puoi autenticarti alla pagina di <a href="php/login">login</a></p>
         <?php } ?>
     </body>
 </html>

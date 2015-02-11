a2e0031fa6b2411dc5d317a2381a75e8bb06d1fd
amm <amm@amm-VirtualBox>
2015-02-11 11:08:00 +0100

    css html php

--- poddaLorenzo/php/view/utente/content.php
+++ poddaLorenzo/php/view/utente/content.php
@@ -5,5 +5,23 @@ switch ($vd->getContentFile()) {
         break;
 
     default:
+        
+        ?>
+        <h2 class="icon-title" id="h-home">Pannello di Controllo</h2>
+        <p>
+            Benvenuto, <?= $user->getNome() ?>
+        </p>
+        <p>
+            Scegli una fra le seguenti sezioni:
+        </p>
+        <ul class="panel">
+            <li><a href="utente/acquista<?= $vd->scriviToken('?')?>" id="pnl-acquista">
+                    Acquista
+                </a>
+            </li>
+            
+        </ul>
+        <?php
+        break;
 }   
 ?>

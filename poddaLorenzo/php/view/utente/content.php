2a0cd28837300d9694191ae9035af1adbe9e1d30
amm <amm@amm-VirtualBox>
2015-02-11 11:17:31 +0100

    css html php

--- poddaLorenzo/php/view/utente/content.php
+++ poddaLorenzo/php/view/utente/content.php
@@ -3,10 +3,10 @@ switch ($vd->getContentFile()) {
     case 'acquisti':
         include_once 'acquisti.php';
         break;
-
-    default:
+?>
+   <?php default: ?>
         
-        ?>
+        
         <h2 class="icon-title" id="h-home">Pannello di Controllo</h2>
         <p>
             Benvenuto, <?= $user->getNome() ?>
@@ -15,7 +15,7 @@ switch ($vd->getContentFile()) {
             Scegli una fra le seguenti sezioni:
         </p>
         <ul class="panel">
-            <li><a href="utente/acquista<?= $vd->scriviToken('?')?>" id="pnl-acquista">
+            <li><a href="utente/acquisti<?= $vd->scriviToken('?')?>" id="pnl-acquista">
                     Acquista
                 </a>
             </li>
--- poddaLorenzo/php/view/venditore/content.php
+++ poddaLorenzo/php/view/venditore/content.php
@@ -11,8 +11,23 @@ switch ($vd->getContentFile()) {
     case 'cancella':
         include_once 'cancella.php';
         break;
-    
-    default:
+    ?>
+    <?php default: ?>
+         <h2 class="icon-title" id="h-home">Pannello di Controllo</h2>
+        <p>
+            Benvenuto, <?= $user->getNome() ?>
+        </p>
+        <p>
+            Scegli una fra le seguenti sezioni:
+        </p>
+        <ul class="panel">
+            
+            <li><a href="venditore/venditore<?= $vd->scriviToken('?')?>" id="pnl-iscrizione">home_venditore</a></li>
+            <li><a href="docente/inserisci<?= $vd->scriviToken('?')?>" id="pnl-libretto">Inserisci</a></li>
+            <li><a href="docente/cancella<?= $vd->scriviToken('?')?>" id="pnl-cerca">Cancella</a></li>
+        </ul>
+        <?php
+        break;
         
 }     
 ?>	

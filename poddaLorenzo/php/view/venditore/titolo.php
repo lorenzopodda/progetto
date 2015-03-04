15aa4843447d896a6e6db6485560e9981e450936
amm <amm@amm-VirtualBox>
2015-03-04 10:37:19 +0100

    css html php

--- poddaLorenzo/php/view/venditore/titolo.php
+++ poddaLorenzo/php/view/venditore/titolo.php
@@ -1,5 +1,5 @@
 
-	<h1>sezione venditore <?= $user->getNome().' '.$user->getCognome() ?> </h1>
+	<h1>Sezione venditore <?= $user->getNome().' '.$user->getCognome() ?> </h1>
 
 <p class="logout">
     <a href="Venditore?cmd=logout">Logout</a>

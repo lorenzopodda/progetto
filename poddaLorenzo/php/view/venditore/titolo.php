8161c590544ddcd5eb2e837687dd7fc99d52d25c
amm <amm@amm-VirtualBox>
2015-03-05 09:32:26 +0100

    css html php

--- poddaLorenzo/php/view/venditore/titolo.php
+++ poddaLorenzo/php/view/venditore/titolo.php
@@ -1,6 +1,6 @@
 
-	<h1>Sezione venditore <?= $user->getNome().' '.$user->getCognome() ?> <a href="http://spano.sc.unica.it/amm2014/davide/esami14/php/login">Logout</a></h1>
+	<h1>Sezione venditore <?= $user->getNome().' '.$user->getCognome() ?> <a href="venditore?cmd=logout">Logout</a></h1>
 
 <p class="logout">
-    <a href="http://spano.sc.unica.it/amm2014/davide/esami14/php/login">Logout</a>
+    <a href="venditore?cmd=logout">Logout</a>
 </p>

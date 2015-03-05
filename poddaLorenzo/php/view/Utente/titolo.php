570ea9bdb28b3a0b27a135d9f8034b2a7e8cc7d4
amm <amm@amm-VirtualBox>
2015-03-05 09:28:41 +0100

    css html php

--- poddaLorenzo/php/view/Utente/titolo.php
+++ poddaLorenzo/php/view/Utente/titolo.php
@@ -3,5 +3,5 @@
 	<h1>Benvenuto utente <?= $user->getNome().' '.$user->getCognome() ?>!!!</h1>
         
 <p>
-    <a href="http://spano.sc.unica.it/amm2014/davide/esami14/php/login">Logout</a>
+    <a href="Utente?cmd=logout">Logout</a>
 </p>
